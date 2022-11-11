4e2a606a0c39b737079c119873e205f618fdcfed
ftwbored <terradweller.ust@gmail.com>
2022-11-11 11:55:09 +0800

    fix height issue

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -2,10 +2,9 @@ import styled from "styled-components";
 
 export const InfoContainer = styled.div`
   color: #fff;
-
-  height: 100vh;
+  height: 120vh;
   display: flex;
-
+  overflow: hidden visible;
   background: ${({ LightBg }) => (LightBg ? "#f9f9f9" : "#fff")};
 
   @media (max-width: 768px) {
